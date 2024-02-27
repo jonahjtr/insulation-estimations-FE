bf7d2b8757ff93f33b8ecdb9633aac4034f4d29f
jonah.jtr <tillmanjonah@gmail.com>
2024-02-26 22:12:13 -0600

    added functionality

--- src/components/estimateMaterial/EstimationForm.jsx
+++ src/components/estimateMaterial/EstimationForm.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 
 const EstimationForm = ({ shownData, setFormData }) => {
+  const [hasNineties, setHasNineties] = useState(false);
   const [formData, setLocalFormData] = useState({
-    pipeCircumference: "",
     pipeLength: "",
+    circumference: "",
+    numberOf90s: "",
+    goreQuantity: "",
   });
-
   const handleChange = (e) => {
     const { name, value } = e.target;
-    const numericValue = parseInt(value); // Parse input value as an integer
+    const numericValue = parseInt(value);
     setLocalFormData({ ...formData, [name]: numericValue || "" });
   };
 
@@ -20,15 +22,14 @@ const EstimationForm = ({ shownData, setFormData }) => {
   return (
     <div className="bg-slate-700 pt-10">
       <form className="max-w-md mx-auto" onSubmit={handleSubmit}>
-        {/* First Name and Last Name Fields */}
         <div className="grid md:grid-cols-2 md:gap-6">
           <div className="relative z-0 w-full mb-5 group">
             <input
               type="text"
               inputMode="numeric"
-              name="pipeCircumference"
-              id="pipeCircumference"
-              value={formData.pipeCircumference}
+              name="circumference"
+              id="circumference"
+              value={formData.circumference}
               onChange={handleChange}
               className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
               placeholder=" "
@@ -36,10 +37,10 @@ const EstimationForm = ({ shownData, setFormData }) => {
             />
             {/* Label */}
             <label
-              htmlFor="pipeCircumference"
+              htmlFor="circumference"
               className="peer-focus:font-medium absolute text-xs text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
-              Pipe Circumference (inc insulation)
+              circumference
             </label>
           </div>
           <div className="relative z-0 w-full mb-5 group">
@@ -61,10 +62,65 @@ const EstimationForm = ({ shownData, setFormData }) => {
               Pipe Length
             </label>
           </div>
+          {hasNineties && (
+            <div>
+              <div className="relative z-0 w-full mb-5 group">
+                <input
+                  type="text"
+                  inputMode="numeric"
+                  name="numberOf90s"
+                  id="numberOf90s"
+                  value={formData.numberOf90s}
+                  onChange={handleChange}
+                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  placeholder=" "
+                  required
+                />
+                {/* Label */}
+                <label
+                  htmlFor="numberOf90s"
+                  className="peer-focus:font-medium absolute text-xs text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                >
+                  numberOf90s
+                </label>
+              </div>
+              <div className="relative z-0 w-full mb-5 group">
+                <input
+                  type="text"
+                  inputMode="numeric"
+                  name="goreQuantity"
+                  id="goreQuantity"
+                  value={formData.goreQuantity}
+                  onChange={handleChange}
+                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  placeholder=" "
+                  required
+                />
+
+                <label
+                  htmlFor="goreQuantity"
+                  className="peer-focus:font-medium absolute text-xs text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                >
+                  goreQuantity
+                </label>
+              </div>
+            </div>
+          )}
         </div>
         <div className="flex justify-center items-center	">
-          <button className=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Click here if you have 90&apos;s.
+          <button
+            onClick={() => setHasNineties(!hasNineties)}
+            className={`text-white ${
+              hasNineties
+                ? "bg-red-800 dark:bg-red-800  dark:hover:bg-red-400	"
+                : "bg-blue-700 dark:bg-blue-600 dark:hover:bg-blue-700"
+            } hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600  dark:focus:ring-blue-800`}
+          >
+            {hasNineties ? (
+              <p>Click here if you DON'T have 90&apos;s.</p>
+            ) : (
+              <p>Click here if you have 90&apos;s.</p>
+            )}
           </button>
         </div>
 
--- src/pages/EstimateMaterial.jsx
+++ src/pages/EstimateMaterial.jsx
@@ -3,8 +3,14 @@ import { useState, useEffect } from "react";
 import EstimationForm from "../components/estimateMaterial/EstimationForm";
 
 const EstimateMaterial = () => {
+  const [cutOutDifficulty, setcutOutDifficulty] = useState("normal");
+  const [circumference, setCircumference] = useState(0);
+  const [pipeLength, setPipeLength] = useState(0);
+  const [ninety, setNinety] = useState({ quantity: 0, goreQuantity: 0 });
+
   const [shownData, setShownData] = useState({});
   const [lengthOfBanding, setLengthOfBanding] = useState("");
+
   console.log("setShownData", shownData);
   const getBandsNeeded = (pipeCirc, pipeLength) => {
     let numberOfBands = pipeLength / 12;
@@ -12,13 +18,76 @@ const EstimateMaterial = () => {
     return bandLength * numberOfBands;
   };
   useEffect(() => {
-    const { pipeCircumference, pipeLength } = shownData;
-    console.log(
-      "getBandsNeeded",
-      getBandsNeeded(pipeCircumference, pipeLength)
-    );
-    setLengthOfBanding(getBandsNeeded(pipeCircumference, pipeLength));
+    const { pipeLength, circumference, numberOf90s, goreQuantity } = shownData;
+    setCircumference(circumference);
+    setPipeLength(pipeLength);
+    setNinety({ quantity: numberOf90s, goreQuantity: goreQuantity });
   }, [shownData]);
+
+  const calculateBandsOf90 = (
+    gores,
+    circumference,
+    numberOf90s,
+    cutOutLevel
+  ) => {
+    const bandLength = circumference + 10;
+    const totalBandLength =
+      gores * bandLength * numberOf90s * findWasteLevel(cutOutLevel);
+
+    return totalBandLength / 12;
+  };
+
+  const findWasteLevel = (cutOutLevel) => {
+    let wasteLevel;
+    if (cutOutLevel === "normal") {
+      wasteLevel = 1.1;
+    } else if (cutOutLevel === "medium") {
+      wasteLevel = 1.15;
+    } else {
+      wasteLevel = 1.2;
+    }
+    return wasteLevel;
+  };
+
+  const calculateBands = (circumference, pipeLength, cutOutLevel) => {
+    const bandLength = circumference + 10;
+    const totalBandsNeeded = pipeLength / 12;
+    const lengthBeforeWaste = bandLength * totalBandsNeeded;
+    const totalBandLength = lengthBeforeWaste * findWasteLevel(cutOutLevel);
+    return totalBandLength / 12;
+  };
+
+  // const circumference = 135;
+  // const pipeLength = 600;
+  // const ninety = {
+  //   quantity: 3,
+  //   goreQuantity: 16,
+  // };
+  // const cutOutDifficulty = "alot";
+  console.log(
+    "straightPipe",
+    calculateBands(circumference, pipeLength, cutOutDifficulty)
+  );
+  console.log(
+    "bands for 90s",
+    calculateBandsOf90(
+      ninety.goreQuantity,
+      circumference,
+      ninety.quantity,
+      cutOutDifficulty
+    )
+  );
+
+  const totalBandLengthForProject =
+    calculateBands(circumference, pipeLength, cutOutDifficulty) +
+    calculateBandsOf90(
+      ninety.goreQuantity,
+      circumference,
+      ninety.quantity,
+      cutOutDifficulty
+    );
+
+  console.log(totalBandLengthForProject);
 
   return (
     <div className="bg-slate-700	h-screen w-screen">
@@ -26,8 +95,80 @@ const EstimateMaterial = () => {
       <div className="mx-auto w-fit ">
         {isNaN(lengthOfBanding) ? <p>result: 0</p> : <p>{lengthOfBanding}</p>}
       </div>
+      <div className="mx-auto w-fit text-slate-300	 ">
+        total Feet Of bands needed:
+        <br />
+        {Math.ceil(totalBandLengthForProject)}
+        <br />
+        Each Band is :{circumference + 10} inches
+        <br />
+        number Of Bands Needed :{Math.ceil(pipeLength / 12)}
+        <br />
+      </div>
     </div>
   );
 };
 
 export default EstimateMaterial;
+
+// console.log("Hello, World!");
+
+// const calculateBandsOf90 = (gores, circumference, numberOf90s, cutOutLevel) => {
+//   const bandLength = circumference + 10;
+//   const totalBandLength =
+//     gores * bandLength * numberOf90s * findWasteLevel(cutOutLevel);
+
+//   return totalBandLength / 12;
+// };
+
+// const findWasteLevel = (cutOutLevel) => {
+//   let wasteLevel;
+//   if (cutOutLevel === "normal") {
+//     wasteLevel = 1.1;
+//   } else if (cutOutLevel === "medium") {
+//     wasteLevel = 1.15;
+//   } else {
+//     wasteLevel = 1.2;
+//   }
+//   return wasteLevel;
+// };
+
+// const calculateBands = (circumference, pipeLength, cutOutLevel) => {
+//   const bandLength = circumference + 10;
+//   const totalBandsNeeded = pipeLength / 12;
+//   const lengthBeforeWaste = bandLength * totalBandsNeeded;
+//   const totalBandLength = lengthBeforeWaste * findWasteLevel(cutOutLevel);
+//   return totalBandLength / 12;
+// };
+
+// const circumference = 135;
+// const pipeLength = 1200;
+// const ninety = {
+//   quantity: 2,
+//   goreQuantity: 14,
+// };
+
+// console.log(
+//   "straightPipe",
+//   calculateBands(circumference, pipeLength, "normal")
+// );
+// console.log(
+//   "bands for 90s",
+//   calculateBandsOf90(
+//     ninety.goreQuantity,
+//     circumference,
+//     ninety.quantity,
+//     "normal"
+//   )
+// );
+
+// const totalBandsForProject =
+//   calculateBands(circumference, pipeLength, "normal") +
+//   calculateBandsOf90(
+//     ninety.goreQuantity,
+//     circumference,
+//     ninety.quantity,
+//     "normal"
+//   );
+
+// console.log(totalBandsForProject);
