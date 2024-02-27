7be1deba0b792da886431b2dff0590ae92e21ca0
Jonah Tillman <tillmanjonah@gmail.com>
2024-02-27 05:47:52 -0600

    Update EstimationForm.jsx

--- src/components/estimateMaterial/EstimationForm.jsx
+++ src/components/estimateMaterial/EstimationForm.jsx
@@ -44,7 +44,7 @@ const EstimationForm = ({ shownData, setFormData }) => {
               htmlFor="circumference"
               className="peer-focus:font-medium absolute text-xs text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
-              circumference
+              circumference (inches)
             </label>
           </div>
           <div className="relative z-0 w-full mb-5 group">
@@ -63,7 +63,7 @@ const EstimationForm = ({ shownData, setFormData }) => {
               htmlFor="pipeLength"
               className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
-              Pipe Length
+              Pipe Length (ft)
             </label>
           </div>
           {hasNineties && (
