4b6691824a906f98728c292a3ce31967cb30f548
jonah.jtr <tillmanjonah@gmail.com>
2024-03-15 16:21:49 -0500

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom";
-import heroPic from "../assets/images/HDplant.png";
+import heroPic from "../assets/images/HDplant.jpg";
 import HeroLinks from "../components/heroLinks/HeroLinks";
 import NavBar from "../components/Nav/NavBar";
 const Home = () => {
   return (
-    <div className="w-[calc(100dvw)] h-[calc(100dvh)] md:h-screen bg-gray-500  overflow-y-hidden	">
+    <div className="w-[calc(100dvw)] h-[calc(100dvh)] md:h-screen bg-gray-500    overflow-y-hidden	">
       <NavBar />
       <div
         className=" w-screen  md:overflow-hidden bg-cover w-full max-w-[3000px] h-full max-h-screen flex justify-center md:items-center	"
         style={{ backgroundImage: `url(${heroPic})` }}
       >
-        <div className="h-fit my-auto py-10 overflow-y-auto flex justify-center items-center  backdrop-blur-xl w-[80vw] sm:min-h-[70vh] max-w-[2000px] md:max-h-[1000px]">
+        <div className="h-fit my-auto py-10 overflow-y-auto flex justify-center items-center  backdrop-blur-xl w-[80vw] sm:min-h-[70vh] max-w-[1000px] md:max-h-[1000px]">
           <HeroLinks />
         </div>
       </div>
